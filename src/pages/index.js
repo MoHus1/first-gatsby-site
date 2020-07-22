1eed6ffac9dbfc26033f0d4b418031a78fdf9c18
MoHus1 <mohus1@alsalamdayschool.org>
2020-07-22 11:05:28 -0500

    Your message about the commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
+      <p>What a world.</p>
+      <img style={{ width: `200px` }} src="https://images.pexels.com/photos/1632790/pexels-photo-1632790.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="" />
+    </div>
+  )
 }
+
+
+
+
